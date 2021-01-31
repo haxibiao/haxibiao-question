84d939255ddb472d4ff0800cb3a682c6cb878f37
ivan <ivan@mac>
2021-01-31 12:44:17 +0800

    标注tags里的fixme

--- database/migrations/2019_02_19_175820_create_tags_table.php
+++ database/migrations/2019_02_19_175820_create_tags_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTagsTable extends Migration
 {
@@ -18,11 +18,11 @@ class CreateTagsTable extends Migration
         }
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->comment('标签名');
-            $table->unsignedInteger('tag_id')->nullable()->index()->comment('标签ID');
+            $table->string('name')->comment('标签名'); //FIXME: 这name应该是unique
+            $table->unsignedInteger('tag_id')->nullable()->index()->comment('标签ID'); //FIXME: 这个字段应该是冗余的
             $table->unsignedInteger('user_id')->nullable()->index()->comment('用户ID');
             $table->unsignedInteger('count')->default(0)->comment('总数');
-            $table->tinyInteger('status')->dafault(0)->comment('状态: -1:删除 0:不可见 1:可见');
+            $table->tinyInteger('status')->dafault(0)->comment('状态: -1:删除 0:默认 1:优先');
             $table->integer('rank')->default(0)->comment('排名');
             $table->string('remark')->nullable()->comment('描述');
             $table->timestamps();
