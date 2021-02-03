fe01c3d8d81604dcc5dc9329a3e58ad5a6b7ffd5
蔡志刚 <caizhigang@haxibiao.com>
2021-02-03 19:51:17 +0800

    clean up migrations

---
+++ database/migrations/2021_02_03_020531_add_fields_to_taggables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('taggables', function (Blueprint $table) {
+            if(!Schema::hasColumn('taggables','user_id')){
+                $table->integer('user_id')->unsigned();
+            }
+            if(!Schema::hasColumn('taggables','tag_name')){
+                $table->string('tag_name');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('taggables', function (Blueprint $table) {
+            //
+        });
+    }
+}
