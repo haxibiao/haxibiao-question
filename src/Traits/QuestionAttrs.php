667f23159322a40f15eee7baf59b846423cd07ae
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-05-14 16:19:46 +0800

    fix:likeattribute重复

--- src/Traits/QuestionAttrs.php
+++ src/Traits/QuestionAttrs.php
@@ -7,17 +7,17 @@ use Haxibiao\Question\Question;
 trait QuestionAttrs
 {
 
-    public function getLikedAttribute()
-    {
-        //检查下是否预加载了,预加载后,则无需重复查询
-        $isPredloaded = isset($this->attributes['liked']);
-        $liked        = $isPredloaded ? $this->attributes['liked'] : false;
-        if (!$isPredloaded && $user = checkUser()) {
-            $liked = $user->isLiked('questions', $this->id);
-        }
+    // public function getLikedAttribute()
+    // {
+    //     //检查下是否预加载了,预加载后,则无需重复查询
+    //     $isPredloaded = isset($this->attributes['liked']);
+    //     $liked        = $isPredloaded ? $this->attributes['liked'] : false;
+    //     if (!$isPredloaded && $user = checkUser()) {
+    //         $liked = $user->isLiked('questions', $this->id);
+    //     }
 
-        return $liked;
-    }
+    //     return $liked;
+    // }
 
     public function setRankAttribute($value)
     {
