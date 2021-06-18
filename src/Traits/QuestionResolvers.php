0d12a80994abaca00ac3b2a1c452f6671c45c5cd
ivan <ivan@haxibiao.com>
2021-06-18 15:16:52 +0800

    整理 resolve方法命名

--- src/Traits/QuestionResolvers.php
+++ src/Traits/QuestionResolvers.php
@@ -39,7 +39,7 @@ trait QuestionResolvers
         return $root->dynamicGold($user);
     }
 
-    public static function resolverNextQuestionCheckpoint($root, array $args, $context, $info)
+    public static function resolveNextQuestionCheckpoint($root, array $args, $context, $info)
     {
         $user         = getUser();
         $wrongCount   = Arr::get($args, 'wrongCount', 0);
@@ -120,33 +120,33 @@ trait QuestionResolvers
         return Question::answerQuestion(getUser(), $question, $args['answer'], $args['time'] ?? 0);
     }
 
-    public function resolverCreateQuestion($root, $args, $c, $i)
+    public function resolveCreateQuestion($root, $args, $c, $i)
     {
         app_track_event('发布', '创建问题');
         $user = getUser();
         return Question::createQuestion($user, $args);
     }
 
-    public function resolverCreateVideoQuestion($root, $args, $c, $i)
+    public function resolveCreateVideoQuestion($root, $args, $c, $i)
     {
         app_track_event('发布', '创建视频题');
         $user = getUser();
         return Question::createVideoQuestion($user, $args);
     }
 
-    public function resolverDeleteQuestion($root, $args, $c, $i)
+    public function resolveDeleteQuestion($root, $args, $c, $i)
     {
         app_track_event('发布', '删除题目');
         return Question::deleteQuestion($args['id']);
     }
 
-    public function resolverRemoveQuestion($root, $args, $c, $i)
+    public function resolveRemoveQuestion($root, $args, $c, $i)
     {
         app_track_event('发布', '撤回题目');
         return Question::removeQuestion($args['id']);
     }
 
-    public function resolverPublishQuestion($root, $args, $c, $i)
+    public function resolvePublishQuestion($root, $args, $c, $i)
     {
         app_track_event('发布', '发布题目');
         return Question::publishQuestion(getUser(), $args['id']);
