eb1485c418d757ef0daef0fbb6cbd0b8c0b4b59b
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-07-30 15:04:34 +0800

    fix:题目评分后，评分人数数据不对

--- src/Traits/QuestionRepo.php
+++ src/Traits/QuestionRepo.php
@@ -42,9 +42,9 @@ trait QuestionRepo
         $questionScore->save();
 
         //计算question得分
-        $question->count_score += 1;
-        $avg_score       = $question->question_scores()->avg('score');
-        $question->score = round($avg_score) == 0 ? 1 : round($avg_score);
+        $question->count_score = $question->question_scores()->count();
+        $avg_score             = $question->question_scores()->avg('score');
+        $question->score       = round($avg_score) == 0 ? 1 : round($avg_score);
         $question->saveDataOnly();
 
         return true;
